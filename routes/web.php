7a4428a834957de647874d9b0f9fef9fe7afeef5
Victor Ferreira <vitu.nff@gmail.com>
2022-10-26 12:24:03 -0300

    user page initiated

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user/{user}', function(\App\Models\User $user) {
-    return $user;
-});
+Route::get('/user/{user}', [\App\Http\Controllers\UserController::class, 'show']);
 
 Route::get('/', function () {
     return view('welcome');
