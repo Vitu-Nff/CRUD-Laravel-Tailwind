546074f644d8a076b4d3150f845caf320658d70c
Victor Ferreira <vitu.nff@gmail.com>
2022-10-26 13:10:38 -0300

    CSS funcionando

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    @stack('styles')
+    <title>@yield('title', 'Estudando Laravel')</title>
+</head>
+<body>
+
+<h1> Meu layout </h1>
+
+@yield('content')
+
+@section('sidebar')
+    <div>
+        <nav>
+            Sidebar Padrão
+        </nav>
+    </div>
+@show
+
+@stack('scripts')
+</body>
+</html>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,1 +1,19 @@
+@extends('layouts.default')
+
+@section('title', 'Layout Usuário')
+
+@section('sidebar')
+<div>
+    <nav>
+        Sidebar do usuário
+    </nav>
+</div>
+@endsection
+
+@section('content')
 <h1>Welcome, {{$user->name}}</h1>
+@endsection
+
+@push('scripts')
+    <script src="{{asset('js/app.js')}}"></script>
+@endpush
